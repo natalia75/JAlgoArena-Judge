12c1029d793aa6db5fa2776ed51c1f3f6d49b4e4
NataliaProkop <natalia.prokop1@gmail.com>
2018-01-25 22:50:54 +0100

    DockerFile

--- src/main/kotlin/com/jalgoarena/JudgeApplication.kt
+++ src/main/kotlin/com/jalgoarena/JudgeApplication.kt
@@ -4,12 +4,30 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.context.annotation.Bean
+
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCaching
+@EnableSwagger2
 open class JudgeApplication
 
 fun main(args: Array<String>) {
     SpringApplication.run(JudgeApplication::class.java, *args)
 }
+
+@Bean
+fun api(): Docket = Docket(DocumentationType.SWAGGER_2)
+        .select() 
+        .apis(RequestHandlerSelectors.basePackage("com.jalgoarena.web"))
+        .paths(PathSelectors.any())                          
+        .build()
