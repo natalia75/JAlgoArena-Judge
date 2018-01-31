af57551ac623226cee9f7795ad324ca916c6b35a
NataliaProkop <natalia.prokop1@gmail.com>
2018-01-31 22:53:23 +0100

    Python compiler - second commit

--- src/main/kotlin/com/jalgoarena/compile/JythonCompiler.kt
+++ src/main/kotlin/com/jalgoarena/compile/JythonCompiler.kt
@@ -1,27 +1,37 @@
+package com.jalgoarena.compile
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+
 class JythonCompiler{
 
-    fun compile() : int{
+    fun compile(path: String) : Int{
 
         try {
             //Runtime rt = Runtime.getRuntime();
             //String[] commands = {"C:\\jython2.7.0\\bin\\jython.exe", "-m compileall", "D:\\PythonT\\"};
             //Process process = rt.exec(commands);
-            val process = Runtime.getRuntime().exec("C:\\jython2.7.0\\bin\\jython.exe -m compileall D:\\PythonT\\")
+            val process = Runtime.getRuntime().exec("C:\\jython2.7.0\\bin\\jython.exe -m compileall ${path}")
             val stdInput = BufferedReader(InputStreamReader(process.getInputStream()))
             val stdError = BufferedReader(InputStreamReader(process.getErrorStream()))
             var s: String? = null
             System.out.println("output")
-            while ((s = stdInput.readLine()) != null) {
+            s = stdInput.readLine()
+            while (s != null) {
                 System.out.println(s)
+                s = stdInput.readLine()
             }
             System.out.println("errors")
-            while ((s = stdError.readLine()) != null) {
+            s = stdError.readLine()
+            while (s != null) {
                 System.out.println(s)
+                s = stdError.readLine()
             }
             System.out.println(process.exitValue())
-
+            return process.exitValue()
         } catch (ex: IOException) {
-            System.out.println(ex.getMessage())
+            System.out.println(ex.message)
+            return 1
         }
 
 
---
+++ src/main/kotlin/com/jalgoarena/compile/PythonCompiler.kt
@@ -0,0 +1,86 @@
+package com.jalgoarena.compile
+import org.jetbrains.kotlin.cli.common.ExitCode
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.PrintStream
+import java.util.*
+
+
+
+class PythonCompiler : JvmCompiler {
+    override fun programmingLanguage() = "python"
+
+    override fun run(className: String, source: String): MutableMap<String, ByteArray?> {
+
+        val tmpDir = createTmpDir()
+        val origErr = System.err
+
+        try {
+            val sourceFile = writeSourceFile(tmpDir, "$className.py", source)
+            val out = File(tmpDir, "out")
+            out.mkdirs()
+
+            val errMessageBytes = ByteArrayOutputStream()
+            System.setErr(PrintStream(errMessageBytes))
+
+            when (compileAndReturnExitCode(tmpDir, sourceFile)) {
+                ExitCode.OK -> return readClassBytes(tmpDir)
+                else -> throw CompileErrorException(errMessageBytes.toString("utf-8"))
+            }
+        } finally {
+            tmpDir.deleteRecursively()
+            System.setErr(origErr)
+        }
+    }
+
+    private fun readClassBytes(out: File): MutableMap<String, ByteArray?> {
+        val classBytes: MutableMap<String, ByteArray?> = HashMap()
+
+        out.listFiles()
+                .filter { it.absolutePath.endsWith(".class") }
+                .forEach {
+                    val byteCode = File(out, it.name).readBytes()
+                    classBytes.put(it.nameWithoutExtension, byteCode)
+                }
+
+        return classBytes
+    }
+
+    private fun compileAndReturnExitCode(sourceFile: File, path: File): ExitCode {
+    var exitCode = 0 
+        try {
+            val jCompiler = JythonCompiler()
+            exitCode = jCompiler.compile(path.absolutePath)
+        } catch (e: Exception) {
+            return ExitCode.COMPILATION_ERROR
+        }
+
+        if(exitCode==0){
+            return ExitCode.OK
+        }
+        return ExitCode.COMPILATION_ERROR
+    }
+
+    private fun processToExitCode(process: Process): ExitCode {
+        return when (process.exitValue()) {
+            0 -> ExitCode.OK
+            1 -> ExitCode.COMPILATION_ERROR
+            3 -> ExitCode.SCRIPT_EXECUTION_ERROR
+            else -> {
+                ExitCode.INTERNAL_ERROR
+            }
+        }
+    }
+
+    private fun createTmpDir(): File {
+        val tmpDir = File("tmp", "${UUID.randomUUID()}")
+        tmpDir.mkdirs()
+        return tmpDir
+    }
+
+    private fun writeSourceFile(tmpDir: File, fileName: String, sourceCode: String): File {
+        val sourceFile = File(tmpDir, fileName)
+        sourceFile.writeText(sourceCode)
+        return sourceFile
+    }
+}
