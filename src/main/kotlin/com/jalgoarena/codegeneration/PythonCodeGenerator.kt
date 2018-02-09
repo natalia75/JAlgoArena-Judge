d8738c7ba453f3bb55ea6883b9c4953965364072
NataliaProkop <natalia.prokop1@gmail.com>
2018-02-09 21:45:09 +0100

    Python added - 5th commit

--- src/main/kotlin/com/jalgoarena/codegeneration/PythonCodeGenerator.kt
+++ src/main/kotlin/com/jalgoarena/codegeneration/PythonCodeGenerator.kt
@@ -7,19 +7,20 @@ class PythonCodeGenerator : JvmCodeGeneration {
     override fun programmingLanguage() = "python"
 
     override fun generateEmptyFunction(function: Function) = """class Solution:
-        ${functionComment(function)}
-        ${functionDeclaration(function)}
-            // Write your code here
-    """
+    ${functionComment(function)}
+    ${functionDeclaration(function)}
+        // Write your code here"""
 
     override fun functionComment(function: Function): String {
-        return """'''${parametersComment(function)}
-        @return ${function.returnStatement.comment}'''"""
+        var retVal = "${parametersComment(function)}"
+        retVal += System.lineSeparator()
+        retVal += "//@return ${function.returnStatement.comment}"
+        return retVal
     }
 
     override fun parametersComment(function: Function): String {
         return function.parameters.map { parameter ->
-            "  @param ${parameter.name} ${parameter.comment}"
+            "//@param ${parameter.name} ${parameter.comment}"
         }.joinToString(System.lineSeparator())
     }
 
